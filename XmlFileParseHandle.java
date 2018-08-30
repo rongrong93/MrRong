986fbcac37691a9d1d380b765fb721d3e6657365
rongrong93 <753182335@qq.com>
2018-08-30 20:41:06 +0800

    java解析xml文件

--- XmlFileParseHandle.java
+++ XmlFileParseHandle.java
@@ -1,6 +1,8 @@
 package com.newland.crm.cmc.biz.sync;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,19 +14,19 @@ import org.dom4j.Element;
 import com.newland.csf.frame.util.XmlUtils;
 
 
-public class XmlFileParseHandle.java {
+public class XmlFileParseHandle {
 	private  Map<Integer,Integer> listSize = new HashMap<Integer,Integer>();
-	public List<Integer> indexList = new ArrayList<Integer>();
+	private List<Object> indexList = new ArrayList<Object>();
 	public static void main(String[] args) throws DocumentException  {
-		new XmlFileParseHandle.java().handle();
+		new XmlFileParseHandle().handle();
 	}
 
-	public void handle() throws DocumentException{
+	public void handle() throws DocumentException{ 
 		StringBuffer sxml = new StringBuffer();
-		sxml.append("<RolBakFile><SvcCont><![CDATA[<?xml version='1.0' encoding='GBK' ?><RolBakChk><ReqDay>20180808</ReqDay><RolBakList><ReqType>0</ReqType><PriMSISDN>13800000000</PriMSISDN><EID>666666</EID><AuxIMEI>dfkg4454548</AuxIMEI><AuxType>12</AuxType><ProfileStatus>01</ProfileStatus><names><name>zhangsan1</name><age>18</age></names><names><name>zhangsan2</name><age>18</age></names><names><name>zhangsan3</name><age>18</age></names><names><name>zhangsan4</name><age>18</age></names><CreateTime>20180808120000</CreateTime></RolBakList><RolBakList><names><name>zhangsan5</name><age>18</age></names><names><name>zhangsan6</name><age>18</age></names><names><name>zhangsan7</name><age>18</age></names><names><name>zhangsan8</name><age>18</age></names><ReqType>1</ReqType><PriMSISDN>13900000000</PriMSISDN><EID>666667</EID><AuxIMEI>dfkg22454548</AuxIMEI><AuxType>1</AuxType><ProfileStatus>0</ProfileStatus><CreateTime>20190808120000</CreateTime></RolBakList></RolBakChk>]]></SvcCont></RolBakFile>");
+		sxml.append("<RolBakFile><a><SvcCont><![CDATA[<?xml version='1.0' encoding='GBK' ?><RolBakChk><ReqDay>20180808</ReqDay><RolBakList><ReqType>0</ReqType><PriMSISDN>13800000000</PriMSISDN><EID>666666</EID><AuxIMEI>dfkg4454548</AuxIMEI><AuxType>12</AuxType><ProfileStatus>01</ProfileStatus><names><name>zhangsan1</name><age>18</age></names><names><name>zhangsan2</name><age>18</age></names><names><name>zhangsan3</name><age>18</age></names><names><name>zhangsan4</name><age>18</age></names><CreateTime>20180808120000</CreateTime></RolBakList><RolBakList><names><name>zhangsan5</name><age>18</age></names><names><name>zhangsan6</name><age>18</age></names><names><name>zhangsan7</name><age>18</age></names><names><name>zhangsan8</name><age>18</age></names><ReqType>1</ReqType><PriMSISDN>13900000000</PriMSISDN><EID>666667</EID><AuxIMEI>dfkg22454548</AuxIMEI><AuxType>1</AuxType><ProfileStatus>0</ProfileStatus><CreateTime>20190808120000</CreateTime></RolBakList></RolBakChk>]]></SvcCont></a></RolBakFile>");
 		//sxml.append("<RolBakFile><SvcCont><![CDATA[<?xml version='1.0' encoding='GBK' ?><RolBakChk><ReqDay>20180808</ReqDay><RolBakList><ReqType>0</ReqType><PriMSISDN>13800000000</PriMSISDN><EID>666666</EID><AuxIMEI>dfkg4454548</AuxIMEI><AuxType>12</AuxType><ProfileStatus>01</ProfileStatus><CreateTime>20180808120000</CreateTime></RolBakList><RolBakList><ReqType>1</ReqType><PriMSISDN>13900000000</PriMSISDN><EID>666667</EID><AuxIMEI>dfkg22454548</AuxIMEI><AuxType>1</AuxType><ProfileStatus>0</ProfileStatus><CreateTime>20190808120000</CreateTime></RolBakList></RolBakChk>]]></SvcCont></RolBakFile>");
 		Map<String, String> rule_map = elementRule2Map();
-		getParaMaps(rule_map,sxml.toString());
+		getParaMaps(rule_map,sxml.toString().trim());
 		System.out.println("---------------finish---------------");
 	}
 	
@@ -33,8 +35,8 @@ public class XmlFileParseHandle.java {
 	 * @param element_rule
 	 * @throws DocumentException
 	 */
-	public static Map<String,String> elementRule2Map() throws DocumentException{
-		String element_rule = "isCDATA:true;CTADAPath:RolBakFile/SvcCont;"
+	private Map<String,String> elementRule2Map() throws DocumentException{
+		String element_rule = "isCDATA:true;CDATAPath:RolBakFile/a/SvcCont;"
 				+ "mainElement:"
 				+ "/RolBakChk/ReqDay,"
 				+ "/RolBakChk/RolBakList/ReqType,"
@@ -52,15 +54,14 @@ public class XmlFileParseHandle.java {
 		 * 获得xml格式配置规则存入map中
 		 */
 		for(String rule : rules){
-			String[] key_value = rule.split(":");
-			String key = key_value[0];
-			String value = key_value[1];
+			String key = rule.split(":")[0];
+			String value = rule.split(":")[1];
 			rule_map.put(key, value);
 		}
 		return rule_map;
 	}
 	//除根节点外,取值最多层数
-	public void  getParaMaps(Map<String,String> rule_map,String xml) throws DocumentException{
+	private void  getParaMaps(Map<String,String> rule_map,String xml) throws DocumentException{
 		String[] main_element = rule_map.get("mainElement").split(",");
 		Map<String,Boolean> paraMap = new HashMap<String,Boolean>();
 		int maxDep = 0;
@@ -70,31 +71,24 @@ public class XmlFileParseHandle.java {
 				maxDep = para.split("/").length - 2;
 			}
 		}
-		Document doc = null;
-		doc = XmlUtils.parseText(xml);
+		Document doc = XmlUtils.parseText(xml);
 		Element rootE = doc.getRootElement();
-		String sxml = ""; 
 		boolean isCDATA = Boolean.parseBoolean(rule_map.get("isCDATA"));
 		if(isCDATA){
-			String[] CTADAPath = rule_map.get("CTADAPath").split("/");
+			String[] cdata_path = rule_map.get("CDATAPath").split("/");
 			//获得CDATA的上一级元素
-			for(int i = 1; i < CTADAPath.length; i ++){
-				rootE = rootE.element(CTADAPath[i]);
+			for(int i = 1; i < cdata_path.length; i ++){
+				rootE = rootE.element(cdata_path[i]);
 			}
-			sxml = rootE.getText();
-		}else{
-			sxml = xml;
+			xml = rootE.getTextTrim();
 		}
-		doc = XmlUtils.parseText(sxml);
-		rootE = doc.getRootElement();
-		for(int i = 0; i < maxDep; i++){
-			indexList.add(-1);
-		}
-//		indexList = Collections.nCopies(maxDep, -1);
-		Map<String,String> resultMap;
 
+		doc = XmlUtils.parseText(xml);
+		rootE = doc.getRootElement();
+		indexList = Arrays.asList(Collections.nCopies(maxDep, -1).toArray());
+		
 		do{
-			resultMap = new HashMap<String,String>();
+			Map<String,String> resultMap = new HashMap<String,String>();
 			System.out.println("==============================");
 			//每次获得一个节点值
 			for(String nodeName : main_element){
@@ -107,7 +101,7 @@ public class XmlFileParseHandle.java {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public void repeatScanNode(Element parentElement, String[] nodeNames, Map<String, String> resultMap,int i)
+	private void repeatScanNode(Element parentElement, String[] nodeNames, Map<String, String> resultMap,int i)
 	{
 		String nodeName = nodeNames[i + 2];
 		List<Element> nextElements = parentElement.elements(nodeName);
@@ -117,11 +111,9 @@ public class XmlFileParseHandle.java {
 			if(!listSize.containsKey(i)){
 				nextElement = nextElements.get(0);
 				listSize.put(i, nextElements.size());
-				if(indexList.get(i) < 0){
-					indexList.set(i, 0);
-				}
+				indexList.set(i, 0);
 			}else{
-				nextElement = nextElements.get(indexList.get(i));
+				nextElement = nextElements.get((Integer)indexList.get(i));
 			}
 			repeatScanNode(nextElement,nodeNames,resultMap,++i);
 		}else if(parentElement.element(nodeName).isTextOnly()){//叶子节点
@@ -143,13 +135,12 @@ public class XmlFileParseHandle.java {
 	/**
 	 * 将最深一层list的索引+1，如果最深层遍历完，置0,上一层list+1
 	 */
-	public boolean doNextList(){
+	private boolean doNextList(){
 		//取最底层list的个数
 		int i = indexList.size() - 1;
-		while(true){
-			if(i < 0 )//遍历至最顶层无list时退出
-				return false;
-			int index = indexList.get(i);//上一次去list的下标
+		
+		while(i >= 0){
+			int index = (int) indexList.get(i);//上一次去list的下标
 			if(index >= 0){
 				//最深一层list是否遍历完，如果没有,下标+1如果遍历完置0,上一层list下标+1
 				if(index < listSize.get(i) - 1){
@@ -165,5 +156,6 @@ public class XmlFileParseHandle.java {
 				i --;
 			}
 		}
+		return false;
 	}
 }
